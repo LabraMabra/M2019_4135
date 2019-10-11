0edad4a84af358c060109517bac9da05520c6077
kat-kuz <44305983+kat-kuz@users.noreply.github.com>
2019-10-11 20:54:40 +0300

    added task02.R

---
+++ Kuzina_Ekaterina/task02.R
@@ -0,0 +1,19 @@
+df_subset <- function(data_frame, rows, columns){
+  df <- data_frame[rows, columns]
+  result = list(df)
+  for (i in c(1:dim(df)[2])) {
+    if (is.numeric(df[, i])) {
+      result[[i+1]] <- mean(df[, i])
+    }
+    else {
+      result[[i+1]] <- table(df[, i])
+    }
+  }
+  return(result)
+}
+
+df = iris
+print(df_subset(df, c(49:52), c('Petal.Length', 'Species')))
+
+df = mtcars
+print(df_subset(df, c(F, F, T), c('cyl', 'gear', 'carb')))
