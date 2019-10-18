9222318f97e993fd86f9996e66b09a3d2a8381f1
LisaSkalon <36557899+LisaSkalon@users.noreply.github.com>
2019-10-18 23:39:37 +0300

    Update task_2.R

--- Skalon_Elizaveta/task_2.R
+++ Skalon_Elizaveta/task_2.R
@@ -1,29 +1,38 @@
-func <- function(df, x1, y1, x2, y2){
-  new_df <- df[x1:y1, x2:y2]
+selection <- function(df, x1, x2){
+  selected_df <- df[x1, x2]
   mean_list <- list()
   freq_list <- list()
-  for (i in 1:ncol(new_df)){
-    if (is.numeric(new_df[,i])==TRUE){
-      mean_list[[names(df)[i]]] <- mean(new_df[,i])
+  # for 1-column situations:
+  if (is.null(ncol(selected_df))==TRUE){
+    selected_df <-data.frame(selected_df)
+  }
+  for (i in 1:ncol(selected_df)){
+    if (is.numeric(selected_df[,i])==TRUE){
+      mean_list[[names(df)[i]]] <- mean(selected_df[,i])
       i <- i+1
     } 
     else {
-      freq_list[[names(df)[i]]] <- table(new_df[,i])
+      freq_list[[names(df)[i]]] <- table(selected_df[,i])
       i <- i+1
     }
   }
-  ret <- list("df" = new_df, "mean" = mean_list, "frequency" = freq_list)
-  return(ret)
+  returned <- list("df" = selected_df, "mean" = mean_list, "frequency" = freq_list)
+  return(returned)
 }
 
 df <- iris
-out_iris<- func(df, 1, 10, 1, 5)
+index_row <- 1:10
+index_col <- 1:5
+out_iris<- selection(df, index_row, index_col)
 out_iris$df
 out_iris$mean
 out_iris$frequency
 
-df1 <- Theoph
-out_Theoph <- func(df1, 1, 20, 1, 4)
-out_Theoph$df1
+df <- Theoph
+index_row <- 1:20
+index_col <- 1:4
+out_Theoph <- selection(df, index_row, index_col)
+out_Theoph$df
 out_Theoph$mean
 out_Theoph$frequency
+
