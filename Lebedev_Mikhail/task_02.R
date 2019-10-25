5705309f90623f62aa6acf1c06b723f85cff564e
boopthesnoot <michanya0109@gmail.com>
2019-10-25 12:46:45 +0300

    HW2 fix

--- Lebedev_Mikhail/task_02.R
+++ Lebedev_Mikhail/task_02.R
@@ -1,4 +1,3 @@
-
 df_subset <- function(dataf, row_select, col_select) {
   subset_df <- dataf[row_select, col_select, drop = FALSE]
   l <- list(subset_df)
@@ -6,7 +5,7 @@ df_subset <- function(dataf, row_select, col_select) {
     if (is.numeric(i)) {
       l <- c(l, sd(i))
     } else {
-      l <- c(l, table(i))
+      l[[length(l)+1]] <- c(table(i))
     }
   }
   return(l)
@@ -15,3 +14,6 @@ df_subset <- function(dataf, row_select, col_select) {
 print(df_subset(mtcars, 1:10, c("mpg", "drat")))
 print(df_subset(mtcars, c("Merc 240D", "Hornet 4 Drive"), c(1,5)))
 print(df_subset(iris, c(T, F, F), "Species"))
+data <- mtcars
+data$gear <- as.factor(data$gear)
+print(df_subset(data, 1:10, 10:11))
