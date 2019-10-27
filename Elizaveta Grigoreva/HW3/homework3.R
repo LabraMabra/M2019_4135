399f10a8e869269420fbbdab7742e1b913fc54cd
lisagrigoreva <55808792+lisagrigoreva@users.noreply.github.com>
2019-10-27 17:49:25 +0300

    Add files via upload

--- Elizaveta Grigoreva/HW3/homework3.R
+++ Elizaveta Grigoreva/HW3/homework3.R
@@ -1,1 +1,15 @@
+cars_data <- cars
+#Identify function and subsetting, check numeric and non-numeric data, remove zeros, merge and return result
+my_function <- function(cars,row,col, math, spliting) {
+  subsetting <- as.data.frame(cars[row,col])
+  fact <- as.factor(subsetting[,spliting])
+  check_numeric <- sapply(subsetting, function(x) if (!is.numeric(x)) table(x))
+  remove_zero <- check_numeric[!sapply(check_numeric, is.null)]
+  split_for_digits <- lapply(split(subsetting[,!names(subsetting) %in% c(spliting)], fact), math)
+  result <- list(subsetting,split_for_digits,remove_zero)
+  return (result )
+} 
+
+
+my_function(mtcars,c("Mazda RX4","Mazda RX4 Wag"),c("hp","carb","disp"),colSums,"disp")
 
