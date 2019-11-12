962d750c8e251720f948a513412c36ec7fc1fcd3
ymogelnitskaya <mogelnitska.ya@mail.ru>
2019-11-12 21:45:16 +0300

    Update task_03.r

--- Mogelnitskaya_Yana/task_03.r
+++ Mogelnitskaya_Yana/task_03.r
@@ -1,20 +1,18 @@
-            f <- function(x) 
-    {
-    if (is.numeric(x)) {
-            g <- sum(x)}
+process_func <- function(x, cfunc) 
+    { if (is.numeric(x)) {
+        return (do.call(cfunc, list(as.matrix(x))))}
     else {
-            g <- summary(x)
-            }
-    return (g)
+        return (summary(x))}
     }
     
-aapp <- function(xfull,row_criteria, col_criteria, split_criteria) # where split_criteria is set as 'desirable column in quotes'
+result_function <- function(xfull,row_criteria, col_criteria, split_criteria, cfunc) # where split_criteria is set as 'desirable column in quotes'
     {
     xf <- subset(xfull, select = c(col_criteria))[row_criteria, ,drop=FALSE]
     spcr <- as.factor(xf[,split_criteria])
-    ls_1 <- lapply(split(xf, spcr, drop =TRUE), function(x){sapply(x,f)})
+    ls_1 <- lapply(split(xf, spcr, drop =TRUE), function(x){ lapply( x, process_func, cfunc) })
     ls <- list(xf, ls_1)
     return (ls)
 }
 
-print(aapp(iris,5:10, 2:5,'Species'))
+print(result_function(iris, 5:10, 2:5,'Species', colSums))
+print(result_function(iris, 5:10, 2:5,'Species', sum))
