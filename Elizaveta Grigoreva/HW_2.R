e110c0903f11376f305a4c7d211b16f6fde1a928
lisagrigoreva <55808792+lisagrigoreva@users.noreply.github.com>
2019-11-14 15:21:30 +0300

    Update HW_2.R

--- Elizaveta Grigoreva/HW_2.R
+++ Elizaveta Grigoreva/HW_2.R
@@ -1,15 +1,9 @@
-#Load different datasets and use one of them 
-data1 <- iris
-data2 <- mtcars
-data3 <- airquality
-
-#Create subsetting function that will take 3 arguments:dataframe, rows, columns and subset it. We need to keep result in the list.
 subsetting  <- function(data2, x, y){
   result <- list()
-  small_set <- data2[x, y]
-#Assign result variable subsetting function and save it into small set
+  small_set <- subset(data2, select=c(x))[y, ,drop=FALSE]
+  #Assign result variable subsetting function and save it into small set
   result$subset <- small_set
-#Start loop to check if columns contain  numeric or non-numerical data. For each element in variable small set. 
+  #Start loop to check if columns contain  numeric or non-numerical data. For each element in variable small set. 
   for (elements in names(small_set)) {
     columns_for_check <- small_set[[elements]]
     checking <- is.numeric(columns_for_check)
@@ -19,9 +13,13 @@ subsetting  <- function(data2, x, y){
       result[[elements]] <- table(columns_for_check, dnn = NULL)     
     }
   }
- return(result)
+  return(result)
 }
 
 #Just for checking
 check <- subsetting(mtcars,5:5,2:1)
 check
+
+data1 <- iris
+data2 <- mtcars
+data3 <- airquality
