307d62f97d8a328f90ac592d5a33de2aba058bc5
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2019-11-17 12:34:22 +0300

    Update HW4.R

--- Mingaleev_Azat/HW4.R
+++ Mingaleev_Azat/HW4.R
@@ -12,12 +12,12 @@ weather <- weather %>%
   spread(measure,value)
 weather$day <- sub("X","",weather$day)
 weather$Events[weather$Events == ""] <- "Sunny"
+# replace empty event on "Sunny"
 weather <- unite(weather,date,date,day,sep = "-")
 # Add to date column days
 all_na <- apply(weather[-(1:3)], MARGIN = 1, FUN = function(x) all(is.na(x)))
-# Not all months have 31 days. In this table "dummy" days have only NA's
 weather <- weather[!all_na,]
-# replace empty event on "Sunny"
+# Not all months have 31 days. In this table "dummy" days have only NA's
 weather[c(-(1:3),-22)] <- apply(weather[c(-(1:3),-22)], MARGIN = 2, FUN = as.numeric)
 # return numeric type for variables
 
