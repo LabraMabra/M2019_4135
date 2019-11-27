0c365a06a2122b0bcb752d24e399fec689d4f192
RezValeri <48764084+RezValeri@users.noreply.github.com>
2019-11-27 12:56:12 +0300

    Update Task4.R

--- Rezapova_Valeriia/Task4.R
+++ Rezapova_Valeriia/Task4.R
@@ -22,5 +22,7 @@ wNA <- which(is.na(weather$Max.Gust.SpeedMPH))
 weather[-c(1,3,22)] <- sapply(weather[-c(1,3,22)], as.numeric)
 #Making as.numeric data with numbers
 weather$Max.Gust.SpeedMPH[wNA] <- sapply(wNA, function(i) with(weather, mean(c(Max.Gust.SpeedMPH[i-5],Max.Gust.SpeedMPH[i+5]))))
-                                         
-                                         
+table(weather$PrecipitationIn)
+#See that we don't have 1
+weather$PrecipitationIn <- as.numeric(weather$PrecipitationIn)
+weather$PrecipitationIn[is.na(weather$PrecipitationIn)] <- sapply(which(is.na(weather$PrecipitationIn)), function(i) with(weather, round(mean(c(PrecipitationIn[i-5],PrecipitationIn[i+5]),na.rm = TRUE),digits=2)))                                         
