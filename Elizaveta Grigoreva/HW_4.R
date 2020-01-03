47cd253821e5d5e535529a4d2f3684ea7aaf5106
lisagrigoreva <55808792+lisagrigoreva@users.noreply.github.com>
2020-01-03 17:37:40 +0300

    Update HW_4.R

--- Elizaveta Grigoreva/HW_4.R
+++ Elizaveta Grigoreva/HW_4.R
@@ -1,23 +1,18 @@
-
 library(data.table)
 library(anchors)
 library (tidyr)
 library (dplyr)
 library (lubridate)
 
-
 weather <- readRDS("/Users/Lisa/Downloads/weather (1).rds")
 weather1 <- dplyr::select(weather, -1)
-weather2 <- gather(weather1 , day, parameter, X1:X31, na.rm = TRUE)
+weather2 <- gather(weather1 , day, val, X1:X31, na.rm = TRUE)
+weather2_x_rem <-  weather2 %>% separate(day, c('del', 'day'), sep ='X') %>% select(-"del")
+weather_common <- unite(weather2_x_rem, date, year, month, day, sep = "-")
+weather_common$date <- as.Date(gsub("X", "", weather_common$date))
+final_df = pivot_wider(weather_common, id_cols = date, names_from = measure, values_from = val)
 
-weather2$day <-  gsub(pattern = "X", replacement = "", x = weather2$day, ignore.case = TRUE)
-weather2  <- mutate(weather2 , parameter=ifelse(parameter=='', 'no_info', parameter))
-weather2 <- unite(weather2 , date, day, month, year, sep='-')
-
-
-weather2$date <-dmy(weather2$date)
-weather2 <- spread(weather2 , measure, parameter)
-weather2 [,-c(1,3,22)] <- sapply(weather2 [, -c(1,3,22)], as.numeric)
-weather_corrected <- weather2 [c(4:21,23)] <- sapply(c(4:21,23), function(x) replace(weather2[,x], which(is.na(weather2[,x])), mean(weather2[,x], na.rm =T)))
-str(weather_corrected)
+final_df$PrecipitationIn <- final_df$PrecipitationIn %>% replace(final_df$PrecipitationIn == 'T', '-1')
+final_df <- final_df %>% mutate(PrecipitationIn = as.numeric(PrecipitationIn))
+View(final_df)
 
