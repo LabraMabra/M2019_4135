650c015206c00cc89ad7217ef34e49841dd312e7
Liuaza Etezova <e7ezova@gmail.com>
2020-01-18 00:39:47 +0300

    Task 06 fix

--- Etezova_Liuaza/task_06.R
+++ Etezova_Liuaza/task_06.R
@@ -1,44 +1,54 @@
+library(tidyr)
 library(ggplot2)
 library(gapminder)
 
-# Part 1
+# Task 06
 # A
-part <- c()
-length <- c()
-width <- c()
-species <- c()
-for(i in 1:nrow(iris)) {
-  index <- i * 2 - 1
-  species[index] <- iris$Species[i]
-  part[index] <- 'Petal'
-  length[index] <- iris$Petal.Length[i]
-  width[index] <- iris$Petal.Width[i]
-  
-  index <- i * 2
-  species[index] <- iris$Species[i]
-  part[index] <- 'Sepal'
-  length[index] <- iris$Sepal.Length[i]
-  width[index] <- iris$Sepal.Width[i]
-}
-
-iris_long <- data.frame('Species' = factor(species, labels=c('setosa', 'versicolor', 'virginica')),
-                        'Part' = part, 'Length' = length, 'Width' = width)
+iris %>%
+  unite(col = Sepal, Sepal.Length, Sepal.Width) %>%
+  unite(col = Petal, Petal.Length, Petal.Width) %>%
+  pivot_longer(c(Sepal, Petal), names_to = "Part", values_to = "Value") %>%
+  separate(col = Value, into = c("Length", "Width"), sep = '_') %>%
+  mutate_at(vars(c(Length, Width)), as.numeric) ->
+  iris_long
 
 ggplot(iris_long, aes(x = Length, y = Width, color = Part)) + 
   geom_point() 
 
-
 # B
-ggplot(gapminder[gapminder$year == 2007, ], aes(x = gdpPercap, y = lifeExp, color = continent)) + 
+ggplot(filter(gapminder, year == 2007), aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
   geom_point() + 
-  geom_line()
+  scale_x_log10()
 
-ggplot(gapminder, aes(x = year, y = pop, color = continent)) + 
-  geom_point()
+gapminder %>%
+  select(-country) %>%
+  group_by(continent, year) %>%
+  mutate(meanLifeExp = mean(lifeExp)) ->
+  gapminder_b
+ggplot(gapminder_b, aes(x = year, y = meanLifeExp, color = continent)) + 
+  geom_point() + 
+  expand_limits(y = 0)
+ggplot(gapminder_b, aes(x = year, y = meanLifeExp, color = continent)) + 
+  geom_line() + 
+  expand_limits(y = 0)
 
 
-# Part 2
+# Task 07
 # 1
-ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, color = continent)) + 
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) + 
   geom_point() +
-  facet_wrap(~ year, nrow = 3, ncol = 4)
+  facet_wrap(~year, nrow = 3, ncol = 4) + 
+  scale_x_log10()
+
+# 2
+airquality %>%
+  pivot_longer(1:4, names_to = "Measure", values_to = "Value") ->
+  airquality_2
+ggplot(airquality_2, aes(x = Day, y = Value, color = Measure)) + 
+  geom_point() + 
+  geom_line() + 
+  facet_grid(Measure~Month, scales = "free")
+
+# 3
+ggplot(airquality, aes(x = Temp, fill = as.factor(Month))) +
+  geom_density(alpha = 0.5)
