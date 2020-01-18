1cd8b05d2ce87b36b44fd106021d9e9fd08b20f5
mfutey <55600426+mfutey@users.noreply.github.com>
2020-01-18 20:37:11 +0300

    Update Task_4.R

--- Futey_Mary/Task_4.R
+++ Futey_Mary/Task_4.R
@@ -1,43 +1,33 @@
-
 library(tidyr)
-library(dplyr)
-library(data.table)
-library(lubridate)
-library(anchors)
-
 
 #load data
-weather <- readRDS("~/Desktop/weather.rds")
+weather_2 <- readRDS("~/Desktop/weather.rds")
+
+#set numeric vector
+numeric <- c(weather[1:20,4], weather[22,4])
 
 #gather days to rows 
 weather_2 <- gather(weather, day, val, X1 : X31, na.rm = TRUE)
 
 #need to remove "X" from day variables
-weather_no_x <- weather_2 %>% separate(day,c('del','day'),sep='X')%>%select(-"del")
+weather_no_x$day <- gsub("X","",weather_no_x$day)
 
 #unite date to one column
-weather_united <- unite(weather_no_x, date, year, month, day, sep="/")
+weather_united <-   unite(weather_no_x, date, day, month, year, sep = "/")
 
-#format date
-weather_united$date <- lubridate::dmy(weather_united$date)
-
-#spread measurements to columns
+#spread columns
 weather_spread <- spread(weather_united, measure, val)
 
-#delete "X" column
-weather_spread$X <- NULL
+#convert to numeric
+weather_final[numeric] <- sapply(weather_final[numeric],as.numeric)
 
-#remove the NA values
-weather_spread_omit <- setDT(weather_spread)[, lapply(.SD, na.omit), by = date]
+#convert T to trace
+weather_final[is.na(weather_final)]<-"Trace"
 
-#convert T to Trace
-weather_trace <- replace.value(weather_spread_omit, "PrecipitationIn", from="T", to="Trace")
+#reorder columns
+weather_final_2 <- weather_final[, c(2, 4, 11, 1, 3, 5:10, 12: 23)]
 
-#check structure
-str(weather_trace)
-
-
-
+View(weather_final_2)
 
 
 
