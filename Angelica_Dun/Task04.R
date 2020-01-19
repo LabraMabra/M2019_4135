c2baa2afc37fc3003cdcc999d3c04c060a3458d2
SmilodonFatalis2696 <55459073+SmilodonFatalis2696@users.noreply.github.com>
2020-01-19 21:22:49 +0300

    Update Task04.R

--- Angelica_Dun/Task04.R
+++ Angelica_Dun/Task04.R
@@ -1,8 +1,12 @@
+
 library(tidyr)
 library(lubridate)
 weather <- readRDS("~/Downloads/weather.rds")
 #first, let's get rid of X in days columns
 names(weather)[5:35] <- 1:31
+#############UPD##########
+measures <- c(weather[1:20,4], weather[22,4])
+
 #all days in one column
 weather_fixed_days <- gather(weather, day, val, 5 : 35, na.rm = TRUE)
 #unite day, month and year in date
@@ -16,6 +20,11 @@ weather_with_date$y_m_d <- ymd(weather_with_date$y_m_d)
 weather_with_date$X <- NULL
 #var-col, obs-row
 weather_spread<- spread(weather_with_date, measure, val)
+
+############UPD################convert all to num
+weather_spread[measures] <- sapply(weather_spread[measures],as.numeric)
+str(weather_spread)
+
 
 #replace NA in Events with meaningful values ("Sunny")
 weather_spread$Events[weather_spread$Events %in% ""] <- "Sunny"
