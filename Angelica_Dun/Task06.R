df425b6b4b76f7a4b0cad5492a75627b2a55165f
SmilodonFatalis2696 <55459073+SmilodonFatalis2696@users.noreply.github.com>
2020-01-19 15:34:09 +0300

    Update and rename Task06_A.R to Task06.R

--- Angelica_Dun/Task06.R
+++ Angelica_Dun/Task06.R
@@ -2,8 +2,8 @@ library(ggplot2)
 library(tidyr)
 library(tidyverse)
 
-
-#long iris
+#A
+#long iris 
 iris_long <- iris  %>% 
   mutate(id = 1:nrow(iris)) %>% #without id 'spread' function wasn't working properly
   gather(flower_part, value, 1:4 ) %>%  #we have 3 columns now: species, flower part(SL, SW, PL, PW) and value 
@@ -14,3 +14,11 @@ iris_long <- iris  %>%
 ggplot(iris_long, aes(x=Width, y=Length, col = flower_part))+
   geom_point(size = .5)
 
+#B
+#2007
+filter(gapminder, year == 2007) %>%
+  ggplot(aes(x= gdpPercap,
+             y = lifeExp,
+             size = pop,
+             color = continent))+
+  geom_point()  +labs(size = "Population", color = "Continent") + scale_x_log10()
